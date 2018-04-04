8d75624f33e374320af50e925ceb3d3eb49abc57
cupdir <cupdir@gmail.com>
2018-04-04 11:22:34 +0800

    new

---
+++ js/analytics.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d545f1c67257ed8de2c4e72f20aef787";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
---
+++ js/page.js
@@ -0,0 +1,172 @@
+// 翻页
+var Page = (function() {
+	var $container = $( '#container' ),
+		$bookBlock = $( '#bb-bookblock' ),
+		$items = $bookBlock.children(),
+		itemsCount = $items.length,
+		current = 0,
+		bb = $( '#bb-bookblock' ).bookblock( {
+			speed : 1,
+			perspective : 2000,
+			shadows:0,
+			shadowSides	: 0,
+			shadowFlip	: 0,
+			onEndFlip : function(old, page, isLimit) {
+				current = page;
+				updateTOC();
+				updateNavigation( isLimit );
+				setJSP( 'init' );
+				setJSP( 'destroy', old );
+
+			}
+		} ),
+		$navNext = $( '#bb-nav-next' ),
+		$navPrev = $( '#bb-nav-prev' ).hide(),
+		$menuItems = $container.find( 'ul.menu-toc > li' ),
+		$tblcontents = $( '#tblcontents' ),
+		transEndEventNames = {
+			'WebkitTransition': 'webkitTransitionEnd',
+			'MozTransition': 'transitionend',
+			'OTransition': 'oTransitionEnd',
+			'msTransition': 'MSTransitionEnd',
+			'transition': 'transitionend'
+		},
+		transEndEventName = transEndEventNames[Modernizr.prefixed('transition')],
+		supportTransitions = Modernizr.csstransitions;
+
+	function init() {
+
+		// initialize jScrollPane on the content div of the first item
+		setJSP( 'init' );
+		initEvents();
+
+	}
+
+	function initEvents() {
+
+		// add navigation events
+		$navNext.on( 'click', function() {
+			bb.next();
+			return false;
+		} );
+
+		$navPrev.on( 'click', function() {
+			bb.prev();
+			return false;
+		} );
+
+		// add swipe events
+		// $items.on( {
+		// 	'swipeleft'		: function( event ) {
+		// 		if( $container.data( 'opened' ) ) {
+		// 			return false;
+		// 		}
+		// 		bb.next();
+		// 		return false;
+		// 	},
+		// 	'swiperight'	: function( event ) {
+		// 		if( $container.data( 'opened' ) ) {
+		// 			return false;
+		// 		}
+		// 		bb.prev();
+		// 		return false;
+		// 	}
+		// } );
+
+		// show table of contents
+		$tblcontents.on( 'click', toggleTOC );
+
+		// click a menu item
+		$menuItems.on( 'click', function() {
+
+			var $el = $( this ),
+				idx = $el.index(),
+				jump = function() {
+					bb.jump( idx + 1 );
+				};
+
+			current !== idx ? closeTOC( jump ) : closeTOC();
+
+			return false;
+
+		} );
+
+		// reinit jScrollPane on window resize
+		$( window ).on( 'debouncedresize', function() {
+			// reinitialise jScrollPane on the content div
+			setJSP( 'reinit' );
+		} );
+
+	}
+
+	function setJSP( action, idx ) {
+
+		var idx = idx === undefined ? current : idx,
+			$content = $items.eq( idx ).children( 'div.content' ),
+			apiJSP = $content.data( 'jsp' );
+
+		if( action === 'init' && apiJSP === undefined ) {
+			$content.jScrollPane({verticalGutter : 0, hideFocus : true });
+		}
+		else if( action === 'reinit' && apiJSP !== undefined ) {
+			apiJSP.reinitialise();
+		}
+		else if( action === 'destroy' && apiJSP !== undefined ) {
+			apiJSP.destroy();
+		}
+
+	}
+
+	function updateTOC() {
+		$menuItems.removeClass( 'menu-toc-current' ).eq( current ).addClass( 'menu-toc-current' );
+	}
+
+	function updateNavigation( isLastPage ) {
+
+		if( current === 0 ) {
+			$navNext.show();
+			$navPrev.hide();
+		}
+		else if( isLastPage ) {
+			$navNext.hide();
+			$navPrev.show();
+		}
+		else {
+			$navNext.show();
+			$navPrev.show();
+		}
+
+	}
+
+	function toggleTOC() {
+		var opened = $container.data( 'opened' );
+		opened ? closeTOC() : openTOC();
+	}
+
+	function openTOC() {
+		$navNext.hide();
+		$navPrev.hide();
+		$container.addClass( 'slideRight' ).data( 'opened', true );
+	}
+
+	function closeTOC( callback ) {
+
+		updateNavigation( current === itemsCount - 1 );
+		$container.removeClass( 'slideRight' ).data( 'opened', false );
+		if( callback ) {
+			if( supportTransitions ) {
+				$container.on( transEndEventName, function() {
+					$( this ).off( transEndEventName );
+					callback.call();
+				} );
+			}
+			else {
+				callback.call();
+			}
+		}
+
+	}
+
+	return { init : init };
+
+})();
