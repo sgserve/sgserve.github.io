25deada555b1b39eba6ece847b9393cb1de6930e
cupdir <cupdir@gmail.com>
2018-04-04 12:39:10 +0800

    abal

--- js/analytics.js
+++ js/analytics.js
@@ -1,7 +1,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d545f1c67257ed8de2c4e72f20aef787";
+  hm.src = "https://hm.baidu.com/hm.js?62f0e26a88703b23fbb13aa1c92fb985";
   var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(hm, s);
 })();
